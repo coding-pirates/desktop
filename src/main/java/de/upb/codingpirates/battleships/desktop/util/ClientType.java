246e0d807fc904cf48c3afb7289ce89909be7734
carolinm <carolinm@campus.upb.de>
2019-12-31 11:17:00 +0100

    added Window for choosing clientType

---
+++ src/main/java/de/upb/codingpirates/battleships/desktop/util/ClientType.java
@@ -0,0 +1,51 @@
+package de.upb.codingpirates.battleships.desktop.util;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ClientType {
+
+    @FXML
+    RadioButton btn_spectator;
+    @FXML
+    RadioButton btn_player;
+    @FXML
+    Label lb_choice;
+
+    public void display() throws IOException {
+        Stage window = new Stage();
+
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle("ClientType");
+
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/ClientTypeView.fxml"));
+        AnchorPane pane = loader.load();
+
+        Image icon = new Image(String.valueOf(ClientType.class.getResource("/images/app_icon.png")));
+        window.getIcons().add(icon);
+
+        Scene scene = new Scene(pane);
+        window.setScene(scene);
+        window.showAndWait();
+    }
+
+    public void player(){
+        lb_choice.setText("Player is choosen");
+    }
+
+    public void spectator(){
+        lb_choice.setText("Spectator is choosen");
+    }
+
+
+
+}
