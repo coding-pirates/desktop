95b04ab4199776d415cd89064bc4c7b276d1df23
carolinm <carolinm@campus.upb.de>
2019-12-31 10:23:49 +0100

    added music

--- src/main/java/de/upb/codingpirates/battleships/desktop/BattleshipsDesktopClientApplication.java
+++ src/main/java/de/upb/codingpirates/battleships/desktop/BattleshipsDesktopClientApplication.java
@@ -7,13 +7,15 @@ import de.upb.codingpirates.battleships.desktop.start.StartController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.Parent;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 import javax.annotation.Nonnull;
 import java.io.IOException;
+import java.net.URL;
 
 public final class BattleshipsDesktopClientApplication extends Application {
 
@@ -52,9 +54,15 @@ public final class BattleshipsDesktopClientApplication extends Application {
         Image icon = new Image(String.valueOf(getClass().getResource("/images/app_icon.png")));
         startStage.getIcons().add(icon);
 
+        final URL resource = getClass().getResource("/raw/sea_of_thieves_theme_song.mp3");
+        final Media media = new Media(resource.toString());
+        final MediaPlayer mediaplayer = new MediaPlayer(media);
+
+
         Scene scene = new Scene(pane);
         startStage.setTitle(TITLE);
         startStage.setScene(scene);
+        mediaplayer.play();
         startStage.show();}
         catch (IOException e){
             e.printStackTrace();
