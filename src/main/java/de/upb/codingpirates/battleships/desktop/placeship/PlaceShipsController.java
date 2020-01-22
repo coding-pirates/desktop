f80bd00856161545cea0eb87745289de72e7ebfa
Roman Lamichhane <rlamich2>
2020-01-22 17:04:38 +0100

    PlaceShipsView Close after GameStart

--- src/main/java/de/upb/codingpirates/battleships/desktop/placeship/PlaceShipsController.java
+++ src/main/java/de/upb/codingpirates/battleships/desktop/placeship/PlaceShipsController.java
@@ -233,6 +233,7 @@ public class PlaceShipsController extends InGameController implements Initializa
             Stage inGameStage = new Stage();
             try {
                 inGame.start(inGameStage,model.getCurrentGame(), ClientType.PLAYER, model.getPlacedShips(),clientId);
+                closeStage();
             } catch (Exception e) {
                 e.printStackTrace();
             }
