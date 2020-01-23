a0bb796e05615792ea46fe60a83938c947d46ab2
Paul Becker <paube@mail.upb.de>
2020-01-23 01:13:04 +0100

    Better startscreen design

--- src/main/java/de/upb/codingpirates/battleships/desktop/start/StartController.java
+++ src/main/java/de/upb/codingpirates/battleships/desktop/start/StartController.java
@@ -8,10 +8,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
-import javax.swing.*;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -23,9 +23,11 @@ public class StartController implements Initializable {
 
     //Views
     @FXML
-    private Button btn_start;
+    Button btn_start;
     @FXML
     ImageView startscreen_imageview;
+    @FXML
+    ImageView startscreen_logoview;
 
     private BattleshipsDesktopClientApplication main;
 
@@ -38,9 +40,28 @@ public class StartController implements Initializable {
         double displayHeight = Screen.getPrimary().getBounds().getHeight();
 
         //used for scaling the background image
-        startscreen_imageview.setImage(new Image("images/startscreen_full_hd.png", displayWidth, displayHeight, true, false));
-        btn_start.setLayoutX(displayWidth/2);
-        btn_start.setLayoutY(displayHeight*0.8);
+        startscreen_imageview.setFitHeight(displayHeight);
+        startscreen_imageview.setFitWidth(displayWidth);
+
+        //start button configuration
+        double btn_startXSize = displayWidth * 414/1920;
+        double btn_startYSize = displayHeight * 120/1080;
+
+        btn_start.setBackground(new Background(new BackgroundImage(new Image(getClass().getResource("/images/button_start_red.png").toExternalForm()), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, new BackgroundSize(btn_startXSize,btn_startYSize,true,true,true,true))));
+        btn_start.setPrefSize(btn_startXSize, btn_startYSize);
+
+        btn_start.setLayoutX(displayWidth * 0.5 - btn_startXSize /2);
+        btn_start.setLayoutY(displayHeight * 0.8 - btn_startYSize/2);
+
+        //logo configuration
+        double logoViewXSize = displayWidth * 900/1920;
+        double logoViewYSize = displayWidth * 800/1080;
+
+        startscreen_logoview.setFitWidth(logoViewXSize);
+        startscreen_logoview.setFitHeight(logoViewYSize);
+        startscreen_logoview.setLayoutX(displayWidth /2 - logoViewXSize/2);
+        startscreen_logoview.setLayoutY(displayHeight *0.7 - logoViewYSize/2);
+
     }
 
     public void setMain(BattleshipsDesktopClientApplication main) {
